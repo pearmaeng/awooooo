87aff63cb977533e12a237798d1dfbf62059316e
Pear Prapawuttikul <pawara.ppp@gmail.com>
2025-02-26 16:02:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
 // The Quirky Website
 
-// On Mouse Over Dog Change to Howling and AWOO appear
-function dogChange() {
-    document.getElementById("dogCollie").src = "img/dog-howling.png"
-    document.getElementById("awooText").innerHTML = "A W O o o o o o O ~"
+// Alert 
+function welcome() {
+    alert("Hello hooman! I am a BORDER COLLIE 🐶. \nPlease unmute to hear me AWOOOO~");
+} 
+window.onload = welcome ;
 
+// Sound is muted at first
+let soundMuted = true; 
+
+function toggleSound() {
+    var muteButton = document.getElementById("mute-button");
     var howlSound = document.getElementById("howling");
-    howlSound.play();
-    howlSound.loop = true;
+
+    // To unlock sound and text on first click 
+    if (soundMuted) {
+        howlSound.play()
+        howlSound.pause();
+        howlSound.currentTime = 0;
+    }
+
+    // For mute/unmute 
+    soundMuted = !soundMuted;
+    muteButton.innerHTML = soundMuted ? "🔇" : "🔊";
+
+    // Stop if Muted
+    if (soundMuted) {
+        howlSound.pause();
+        howlSound.currentTime = 0;
+    }
+
 }
 
+// On Mouse Over Dog Change, Howling on, and AWOO appear
+function dogChange() {
+    var howlSound = document.getElementById("howling");
+
+    document.getElementById("dogCollie").src = "img/dog-howling.png";
+
+    if (!soundMuted) {
+        document.getElementById("awooText").innerHTML = "A W O o o o o o O ~";
+        howlSound.loop = true;
+        howlSound.currentTime = 0;
+        howlSound.play();
+    }
+}
+
+// Mouse away and everything back to original
 function dogReturn() {
-    document.getElementById("dogCollie").src = "img/dog.png"
-    document.getElementById("awooText").innerHTML = ""
+    document.getElementById("dogCollie").src = "img/dog.png";
+    document.getElementById("awooText").innerHTML = "";
 
     var howlSound = document.getElementById("howling");
     howlSound.pause();
